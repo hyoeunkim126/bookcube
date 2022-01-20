2f84a3079529ab01126970c164feec3e1ada7bae
hyoeunkim126 <93234816+hyoeunkim126@users.noreply.github.com>
2022-01-20 17:43:50 +0900

    0120

---
+++ js/category.js
@@ -0,0 +1,12 @@
+// category 모달팝업
+$(function(){
+    $('.all-category').click(function(){
+      $('#all-category-popup').show();
+    });
+  
+  
+    $('.close').click(function(){
+      $('#all-category-popup').hide();
+    });
+  });
+  
