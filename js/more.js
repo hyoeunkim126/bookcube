05b6e890bab18caddf3ef02852fcbd2b7b2e26ab
hyoeunkim126 <hyoeun126@naver.com>
2022-01-26 15:13:50 +0900

    0126

--- js/more.js
+++ js/more.js
@@ -1,20 +1,10 @@
 // 더보기 이벤트
 $(function(){
   var tImg = $(".img-wrap img").height();
-  if(tImg < 340){
+  if(tImg < 330){
     $(".i-more").hide();
   }
   $(".i-more").on("click",function(){
-    $(this).prev().addClass("on");
-    $(this).hide();
-  });	
-});
-$(function(){
-  var tText = $(".t-content").height();
-  if(tText < 150){
-    $(".t-more").hide();
-  }
-  $(".t-more").on("click",function(){
     $(this).prev().addClass("on");
     $(this).hide();
   });	
